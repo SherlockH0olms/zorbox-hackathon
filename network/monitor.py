07ad0a92c8190e4e09de3dc2c3159796c47f788e
simralhuseynli1-stack <simralhuseynli1@gmail.com>
2025-11-01 14:52:54 +0400

    Create monitor.py

---
+++ network/monitor.py
@@ -0,0 +1,7 @@
+class NetworkMonitor (docker logs parsing + tcpdump)
+
+def generate_network_report(self, job_id)
+
+if __name__ == "__main__": main()
+
+Dəyişiklik: /var/log/zorbox/monitor_reports kimi system path istifadə olunur, sudo ilə işlədilməlidir.
